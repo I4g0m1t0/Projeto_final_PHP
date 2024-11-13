82b3e80fb4c9162c95074a7595acc979ea3d069a
Iago <iagorechtramontin@gmail.com>
2024-11-13 14:42:34 -0300

    Tela inicial foi passada de html e css para PHP

---
+++ Tela_inicial_php/assets/includes/components/navbar.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar">
+        <div class="menu-left">
+            <i class="fa-solid fa-bars" onclick="openmenu()"></i>
+            <a href="<?php echo $indexRoute; ?>">
+                <img src="<?php echo $imageFile; ?>" alt="Senac_logo">
+            </a>
+        </div>
+        <div class="menu-right">
+            <i class="fa-solid fa-envelope"></i>
+            <i class="fa-solid fa-bell"></i>
+        </div>
+    </nav>
+</header>
---
+++ Tela_inicial_php/assets/includes/components/sidebar.php
@@ -0,0 +1,23 @@
+<div class="content">
+        <div class="sidebar">
+            <a href="<?php echo $rematriculaRoute; ?>">
+                <div class="menu">
+                    <i class="fa-sharp fa-solid fa-user-graduate" href="pages/rematricula.html"></i>
+                </div>  
+            </a>
+            <a href="<?php echo $documentosRoute; ?>">
+                <div class="menu">
+                    <i class="fa-solid fa-file"></i>
+                </div>
+            </a>
+            <a href="<?php echo $financeiroRoute; ?>">    
+                <div class="menu">
+                    <i class="fa-solid fa-money-bill-transfer"></i>
+                </div>
+            </a>
+            <a href="<?php echo $diplomaRoute; ?>">
+                <div class="menu">
+                    <i class="fa-solid fa-qrcode"></i>
+                </div>
+            </a>
+        </div>
